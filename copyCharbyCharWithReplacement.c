2ab2b9ac61d977f61e1a532e78a287fc78d3dd6a
Ryan Davis <ryandavisim@gmail.com>
2024-05-11 23:42:36 -0400

    Added Exercise 1.10 into a different file

--- copyCharbyCharWithReplacement.c
+++ copyCharbyCharWithReplacement.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 /*
-Exercise 1.9: Write a program to copy its input to its output, replacing each string of 1 or more blanks with a single blank
-Exercise 1.10: Write a program to copy its input to its output, replacing each tab by \t, each backspace by \b and each backslash by \\
+Exercise 1.9: Write a program to copy its input to its output,
+replacing each string of 1 or more blanks with a single blank
+
 */
 main()
 {
